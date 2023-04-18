6171b224b8c70ce801248b5cc7cf156b77e5b913
Sasha Afanasyev <afonia.marcii@gmail.com>
2023-04-18 20:26:58 +0300

    feat: add js folder & tags filter code  (Tuesday, April 18, 2023 5:26:58 PM)

---
+++ src/index.js
@@ -0,0 +1,50 @@
+window.onload = function () {
+  addTagsClickHandler();
+};
+
+const addTagsClickHandler = () => {
+  document.querySelector('.strategies__tags').addEventListener('click', (e) => {
+    if (e.target.classList.contains('tag')) {
+      let clickedTag = e.target;
+      removeSelectedTags();
+      selectClickedTag(clickedTag);
+    }
+  });
+};
+
+const removeSelectedTags = () => {
+  let tags = document.querySelectorAll('.strategies__tags .tag');
+  tags.forEach((tag) => {
+    tag.classList.remove('tag_selected');
+    tag.classList.add('tag_bordered');
+    if (clickedTag.innerText === 'All') {
+      showAllStrategies();
+    } else {
+      filterStrategyBySelectedTag(clickedTag.innerText);
+    }
+  });
+};
+
+const selectClickedTag = (clickedTag) => {
+  clickedTag.classList.add('tag_selected');
+  clickedTag.classList.remove('tag_bordered');
+};
+
+const showAllStrategies = () => {
+  let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+  strategies.forEach((strategy) => {
+    strategy.classList.remove('strategy_hidden');
+  });
+};
+
+const filterStrategyBySelectedTag = (selectedTag) => {
+  let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+  strategies.forEach((strategy) => {
+    strategy.classList.add('strategy_hidden');
+    strategy.querySelectorAll('.tag').forEach((tag) => {
+      if (tag.innerText === selectedTag) {
+        strategy.classList.remove('strategy_hidden');
+      }
+    });
+  });
+};
